d6fb6dd9ce1196018d70ddfbcda5f2861b050843
lifei <876451336@qq.com>
2021-06-18 17:46:04 +0800

    添加数据库配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module, Global } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { AppController } from '@/app.controller'
 import { AppService } from '@/app.service'
 
+//全局依赖模块
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { WebInitModule } from '@/web-module/init/init.module'
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), WebInitModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forFeature([]), WebInitModule],
 	controllers: [AppController],
 	providers: [AppService]
 })
