f66839ee9efd757204961c2c259c02f063b1e19a
fanyanghua <876451336@qq.com>
2021-06-22 08:11:11 +0800

    根模块

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('根模块')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	@Get()
+	getHello(): string {
+		return this.appService.getHello()
+	}
 }
