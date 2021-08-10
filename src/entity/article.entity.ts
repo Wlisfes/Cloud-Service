46b07deee29b14d11aac467fe05fb173cff302e6
lifei <876451336@qq.com>
2021-08-10 18:28:04 +0800

    新增文章表

--- src/entity/article.entity.ts
+++ src/entity/article.entity.ts
@@ -4,9 +4,30 @@ import { UserEntity } from '@/entity/user.entity'
 import { SourceEntity } from '@/entity/source.entity'
 
 @Entity('article')
-export class ArticleEntity {
+export class ArticleEntity extends DateEntity {
 	@PrimaryGeneratedColumn({ comment: '自增长主键' })
 	id: number
+
+	@Column({ nullable: false, comment: '文章标题' })
+	title: string
+
+	@Column({ nullable: false, comment: '文章封面' })
+	cover: string
+
+	@Column({ type: 'text', nullable: false, comment: '文章内容' })
+	content: string
+
+	@Column({ nullable: true, default: null, comment: '原文连接' })
+	url: string
+
+	@Column({ nullable: false, default: 1, comment: '文章状态: 0.禁用 1.启用 2.删除' })
+	status: number
+
+	@Column({ nullable: false, default: 0, comment: '排序号' })
+	order: number
+
+	@Column({ nullable: false, default: 0, comment: '浏览量' })
+	browse: number
 
 	@ManyToOne(type => UserEntity)
 	user: UserEntity
--- src/module/article/article.service.ts
+++ src/module/article/article.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { ArticleEntity } from '@/entity/article.entity'
+import { SourceEntity } from '@/entity/source.entity'
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+	constructor(
+		@InjectRepository(ArticleEntity) private readonly articleModel: Repository<ArticleEntity>,
+		@InjectRepository(SourceEntity) private readonly sourceModel: Repository<SourceEntity>
+	) {}
+
+	/**创建文章**/
+	public async nodeCreateArticle() {
+		try {
+			return { message: '创建成功' }
+		} catch (e) {
+			throw new HttpException(e.message || e.toString(), HttpStatus.BAD_REQUEST)
+		}
+	}
+}
